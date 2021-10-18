719deaa1bc907db3e4c11449708fdabce53496b1
RizlaneAbalil <38002332@parisnanterre.fr>
2021-10-18 16:20:41 +0200

    exercice 3 - Généralisation - Question 3 : Update class "Chaine"

--- app/src/main/java/PF_TD1/ArbresGeneralises2/Chaine.java
+++ app/src/main/java/PF_TD1/ArbresGeneralises2/Chaine.java
@@ -1,9 +1,21 @@
-class Chaine
+package PF_TD1.ArbresGeneralises2;
+
+class Chaine implements Sommable<Chaine>
 {
 	private String valeur; 
 
 	public Chaine(String valeur)
 	{
 		this.valeur = valeur; 
-	} 
+	}
+
+	public String valeurs()
+	{
+		return this.valeur;
+	}
+
+	@Override
+	public Chaine sommer(Chaine valeur) {
+		return new Chaine(this.valeurs()+valeur.valeurs());
+	}
 }
