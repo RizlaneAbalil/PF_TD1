ca66b196852c3f8ccbaf5688a1566def4a55502a
RizlaneAbalil <38002332@parisnanterre.fr>
2021-10-18 16:49:42 +0200

    exercice 3 - Généralisation - Question 4 : Update interface "Arbre" methode somme

--- app/src/main/java/PF_TD1/ArbresGeneralises2/Arbre.java
+++ app/src/main/java/PF_TD1/ArbresGeneralises2/Arbre.java
@@ -1,11 +1,19 @@
-public interface Arbre<T>
+package PF_TD1.ArbresGeneralises2;
+
+import PF_TD1.ArbresGeneralises2.Sommable;
+
+import java.util.Set;
+
+public interface Arbre<T extends Sommable<T>>
 {
     int taille(); // nombre de valeurs
+
     //Avant on passait des integers mais maintenant le type peut être générique (voitures, int, string ...)
-    boolean contient(final T  val); // vrai si val est contenue dans l'arbre,
-    faux sinon
+    boolean contient(final T  val); // vrai si val est contenue dans l'arbre,faux sinon
+
     Set<T> valeurs(); // ensemble des valeurs différentes dans l'arbre
-    //Integer somme(); // somme des valeurs
+
+    T somme(); // somme des valeurs
     // Integer min(); // valeur minimale
     //Integer max(); // valeur maximale
     //boolean estTrie(); // vrai si l'arbre est trié, faux sinon
