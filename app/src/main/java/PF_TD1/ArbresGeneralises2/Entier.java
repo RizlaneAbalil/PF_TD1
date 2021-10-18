e8e11aa693f5553ad3dfb1acb26d04be1cf0fd03
RizlaneAbalil <38002332@parisnanterre.fr>
2021-10-18 16:18:42 +0200

    exercice 3 - Généralisation - Question 3 : Update class "Entier"

--- app/src/main/java/PF_TD1/ArbresGeneralises2/Entier.java
+++ app/src/main/java/PF_TD1/ArbresGeneralises2/Entier.java
@@ -1,9 +1,22 @@
-class Entier
+package PF_TD1.ArbresGeneralises2;
+
+class Entier implements Sommable<Entier>
 {
 	private int valeur; 
 
 	public Entier(int valeur)
 	{
 		this.valeur = valeur;
+	}
+
+	//getteur
+	public int valeurs()
+	{
+		return this.valeur;
+	}
+
+	@Override
+	public Entier sommer(Entier valeur) {
+		return new Entier(this.valeurs() + valeur.valeurs());
 	}
 }
